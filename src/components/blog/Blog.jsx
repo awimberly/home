c395d2bd9f3e9eae59f7cef08f641777d44d8ef7
awimberly <awimberly@comptia.global>
2025-05-26 17:40:28 -0500

    chore: Update blog imports to fix build error.

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -2,46 +2,47 @@ import React from "react";
 import { BlogBuilder } from "./BlogBuilder";
 import bloglist from "../../editable-stuff/blog";
 import { Link } from "react-router-dom";
-const Blog = (props) => {
+
+const Blog = () => {
   return (
     <div className="container-lg mt-5 bg-blue">
       <h1 className="text-center">Blogs</h1>
-      {bloglist.map((value, index) => {
-        return (
-          <BlogCard
-            key={index}
-            title={value.title}
-            description={value.description}
-            index={index}
-          />
-        );
-      })}
+      {bloglist.map((value, index) => (
+        <BlogCard
+          key={index}
+          title={value.title}
+          description={value.description}
+          index={index}
+        />
+      ))}
     </div>
   );
 };
 
-const BlogCard = ({ index, title, image, description }) => {
+const BlogCard = ({ index, title, description }) => {
   return (
     <div className="m-5">
-      <div className="">
-        <div className="row">
-          <div className="col-4 col-lg-12">
-            {/* <img src={image} className="card-img" alt="..." /> */}
-          </div>
-          <div className="col-8 col-lg-12">
-            <div className="">
-              <h1 className="">{title}</h1>
-              <p className="lead">{description}</p>
-              <Link to={`${process.env.PUBLIC_URL}blog/${index}`}>
-                Read more...{" "}
-              </Link>
-            </div>
+      <div className="row">
+        <div className="col-4 col-lg-12">
+          {/* <img src={image} className="card-img" alt="..." /> */}
+        </div>
+        <div className="col-8 col-lg-12">
+          <div>
+            <h1>{title}</h1>
+            <p className="lead">{description}</p>
+            <Link to={`${process.env.PUBLIC_URL}/blog/${index}`}>
+              Read more...
+            </Link>
           </div>
         </div>
-        <hr />
       </div>
+      <hr />
     </div>
   );
 };
 
-export { Blog, BlogBuilder };
+// ✅ Default export for main Blog component
+export default Blog;
+
+// ✅ Named export if you still use BlogBuilder elsewhere
+export { BlogBuilder };
